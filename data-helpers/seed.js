17d707b82d83506c6171b98542e2098329cee8be
Daniel Bennington <dbennington125@gmail.com>
2020-06-25 14:27:48 -0700

    were committing

--- data-helpers/seed.js
+++ data-helpers/seed.js
@@ -1,12 +1,22 @@
 require('dotenv').config();
 const chance = require('chance').Chance();
 const Reviewer = require('../lib/models/Reviewer');
+const Studio = require('../lib/models/Studio');
 
-const seed = async({ reviewer = 3 } = {}) => {
+const seed = async({ reviewers = 3, studios = 5 } = {}) => {
 
-  const createdReviewer = await Reviewer.create([...Array(reviewer)].map(() => ({
+  const createdReviewers = await Reviewer.create([...Array(reviewers)].map(() => ({
     name: chance.name(),
     company: chance.company()
+  })));
+
+  const createdStudios = await Studio.create([...Array(studios)].map(() => ({
+    name: chance.name(),
+    address: {
+      city: chance.city(),
+      state: chance.state(),
+      country: chance.country()
+    }
   })));
 
 };
